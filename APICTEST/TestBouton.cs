af04d08e16246a182c6d8be55657bb81af447954
Jeremy DON <jeremy.don@gmx.com>
2024-03-24 16:32:21 +0100

    Test intégration ok, modification du style du boutton dans les détails du favoris

--- APICTEST/TestBouton.cs
+++ APICTEST/TestBouton.cs
@@ -1,29 +1,23 @@
 ﻿using Bunit;
-using RichardSzalay.MockHttp;
-using Microsoft.Extensions.DependencyInjection;
-using APIC.Pages;
 
-public class CatImageComponentTests : TestContext
+public class ImageDetailIntegrationTests : TestContext
 {
     [Fact]
-    public void NewCatImage_IsLoadedOnClick()
+    public void ImageDetailComponent_RendersCorrectly()
     {
         // Arrange
-        var mockHttp = new MockHttpMessageHandler();
-        mockHttp.When("https://cataas.com/cat?json=true")
-                .Respond("application/json", "{\"id\":\"abc\",\"url\":\"https://cataas.com/cat/abc\",\"tags\":[\"cute\"],\"createdAt\":\"\",\"updatedAt\":\"\",\"mimetype\":\"image/jpeg\",\"size\":123}"); // Réponse JSON simulée
+        var jsInterop = this.JSInterop.Setup<String>("getFavoriteImages");
+        jsInterop.SetResult("[]");
 
-        Services.AddSingleton<HttpClient>(new HttpClient(mockHttp));
-
-        var component = RenderComponent<Home>();
+        var testImageId = "testImage123";
 
         // Act
-        component.Find("#call").Click();
+        var component = RenderComponent<APIC.Pages.ImageDetail>(parameters =>
+        {
+            parameters.Add(p => p.ImageId, testImageId);
+        });
 
         // Assert
-        var imgElement = component.Find("img");
-        Assert.NotNull(imgElement);
-        Assert.Equal("https://cataas.com/cat/abc", imgElement.GetAttribute("src")); 
-        Assert.Equal("cute", imgElement.GetAttribute("alt"));
+        Assert.NotEmpty(component.Markup);
     }
 }
